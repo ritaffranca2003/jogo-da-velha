ea4143197965f1f722847974825514638e7ac58c
Rita Franca <105826740+ritaffranca2003@users.noreply.github.com>
2022-10-26 20:49:52 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var Jogador1;
+var Jogador1;
+
+function Escolha1(){
+    Jogador1 = "1";
+}
+function Escolha2(){
+    Jogador1 = "2";
+}
+function Escolha3(){
+    Jogador1 = "3";
+}
+function Escolha4(){
+    Jogador2 = "1";
+}
+function Escolha5(){
+    Jogador2 = "2";
+}
+function Escolha6(){
+    Jogador2 = "3";
+}
